3d6a331e68ed080774ca752d42b72d51acbc6928
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-08-12 10:07:43 +0700

    Solve Stones on the Table

---
+++ java/src/main/java/com/bintang/java/codingchallenges/StonesOnTheTable.java
@@ -0,0 +1,37 @@
+package com.bintang.java.codingchallenges;
+
+public class StonesOnTheTable {
+
+    /*There are some stones on Bob's table in a row, and each of them can be red, green or blue, indicated by the characters R, G, and B.
+    Help Bob find the minimum number of stones he needs to remove from the table so that the stones in each pair of adjacent stones have different colors.
+    Examples:
+            "RGBRGBRGGB"   => 1
+            "RGGRGBBRGRR"  => 3
+            "RRRRGGGGBBBB" => 9*/
+
+    public static void main(String[] args) {
+
+        System.out.println(countRemovedStone("RRRRGGGGBBBB"));
+
+    }
+
+    public static int countRemovedStone(String stones) {
+
+        int countRemovedStone = 0;
+        stones = stones.toLowerCase();
+        char[] kataArray = new char[stones.length()];
+
+        for (int i = 0; i < stones.length(); i++) {
+            kataArray[i] = stones.charAt(i);
+        }
+
+        for (int i = 0; i < kataArray.length-1; i++) {
+            if (kataArray[i] == kataArray[i + 1]) {
+                countRemovedStone += 1;
+            }
+        }
+
+        return countRemovedStone;
+    }
+
+}
