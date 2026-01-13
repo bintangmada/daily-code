102ab6c85132a2da82d43ef5d2b3ae64f2c2eef1
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-01-13 10:53:12 +0700

    regex remove duplikat kata

---
+++ java/src/main/java/com/bintang/java/experiment/MyMain.java
@@ -0,0 +1,9 @@
+package com.bintang.java.experiment;
+
+public class MyMain {
+    public static void main(String[] args) {
+        String kata = "Saya";
+
+        System.out.println(kata.charAt(1));
+    }
+}
